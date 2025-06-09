49ce7626269e2256c8b6ec27bf35e461fae54ee8
bnjmnt <dev@bnjmnt.com>
2025-06-08 23:15:44 -0700

    Change template naming

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./content2html.sh index.html.content page.html.template > index.html
-./content2html.sh contact.html.content page.html.template > contact.html
+./content2html.sh index.content.html page.template.html > index.html
